7cc3b39bc8485990ee8ac1a2b63aba15c562badd
Matheus Bereta <matheus_bereta@hotmail.com>
2020-03-04 16:34:53 -0300

    :sparkles: :white_check_mark: Including data provider example

---
+++ src/main/java/br/com/matheus/datadriven/DocumentationDataProvider.java
@@ -0,0 +1,15 @@
+package br.com.matheus.datadriven;
+
+import lombok.experimental.UtilityClass;
+import org.testng.annotations.DataProvider;
+
+@UtilityClass
+public class DocumentationDataProvider {
+
+    @DataProvider(name = "documentation")
+    public static Object[][] getDocumentationItems() {
+        return new Object[][] {
+                {"Grid"}
+        };
+    }
+}
--- src/test/java/br/com/matheus/tests/AboutGridTest.java
+++ src/test/java/br/com/matheus/tests/AboutGridTest.java
@@ -1,6 +1,7 @@
 package br.com.matheus.tests;
 
 import br.com.matheus.common.BaseTest;
+import br.com.matheus.datadriven.DocumentationDataProvider;
 import br.com.matheus.pages.DocumentationPage;
 import br.com.matheus.pages.HomePage;
 import org.testng.annotations.Test;
@@ -9,10 +10,8 @@ import static org.testng.Assert.*;
 
 public class AboutGridTest extends BaseTest {
 
-    String searchInput = "Grid";
-
-    @Test
-    public void validateDocumentationAboutGrid() {
+    @Test(dataProvider = "documentation", dataProviderClass = DocumentationDataProvider.class)
+    public void validateDocumentationAboutGrid(String searchInput) {
         DocumentationPage documentation = new HomePage()
                 .accessMenuDocumentation()
                 .searchDocumentation(searchInput)
